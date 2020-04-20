f5b1953d17c5f814e3c55e9aaa29b9a36a134989
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-04-20 15:28:39 +0900

    [finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
 
   #/todokists/:id = /todolists/1や.../todolists/3などに該当する
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
+
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 end
+
